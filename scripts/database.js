7950d6372321c49ea8e21c2d51bb7da4c4c99827
JoshRittenberry <joshrittenberry96@gmail.com>
2023-08-15 15:41:27 -0500

    Starting my Database

---
+++ scripts/database.js
@@ -0,0 +1,34 @@
+cosnt database = {
+    fish: [
+        {
+            species: "Pterophyllum scalare",
+            name: "Charlie",
+            location: "South America",
+            food: "Small Crustaceans",
+        },
+        {
+            species: "Carassius auratus",
+            name: "Goldie",
+            location: "East Asia",
+            food: "Fish Flakes",
+        },
+        {
+            species: "Symphysodon discus",
+            name: "Saucy Boi",
+            location: "South America",
+            food: "Fish Flakes",
+        },
+        {
+            species: "Poecilia sphenops",
+            name: "Polly",
+            location: "North & South America",
+            food: "Pellets",
+        },
+        {
+            species: "Poecilia reticulata",
+            name: "Gupps",
+            location: "South America",
+            food: "Brine Shrimp",
+        },
+    ]
+}
